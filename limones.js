c3b7e958aed6be079e11929e992877ad9f72bfe8
Goshe~ARTS <chavezramosevanismael6@gmail.com>
2026-09-15 16:01:02 -0500

    - agregadas funciones de movimiento, limpiarCanvas y actualizarPantalla -

--- limones.js
+++ limones.js
@@ -5,6 +5,16 @@ const ALTURA_SUELO=25
 const ALTURA_PERSONAJE=55
 const ACNCHO_PERSONAJE=35
 
+let personajeX=canvas.clientWidth/2 
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.clientWidth,canvas.clientHeight)
+}
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
@@ -15,5 +25,13 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="#265E6E";
-    ctx.fillRect(canvas.clientWidth/2,canvas.clientHeight-(ALTURA_SUELO+ALTURA_PERSONAJE),ACNCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,canvas.clientHeight-(ALTURA_SUELO+ALTURA_PERSONAJE),ACNCHO_PERSONAJE,ALTURA_PERSONAJE)
+}
+function moverIzquierda(){
+    personajeX=personajeX-20
+    actualizarPantalla();
+}
+function moverDerecha(){
+    personajeX=personajeX+20
+    actualizarPantalla();
 }
