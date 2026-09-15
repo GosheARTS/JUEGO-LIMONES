2058aecc78c1298abcf0df02e9fd2a4d8239b1af
Goshe~ARTS <chavezramosevanismael6@gmail.com>
2026-09-15 18:54:55 -0500

    - agregadas funciones dibujarLimon y bajarLimon -

--- limones.js
+++ limones.js
@@ -4,8 +4,12 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=25
 const ALTURA_PERSONAJE=55
 const ACNCHO_PERSONAJE=35
+const ANCHO_LIMON=15
+const ALTO_LIMON=15
 
 let personajeX=canvas.clientWidth/2 
+let limonX=canvas.clientWidth/2;
+let limonY=5;
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.clientWidth,canvas.clientHeight)
@@ -14,10 +18,12 @@ function actualizarPantalla(){
     limpiarCanvas();
     dibujarPersonaje();
     dibujarSuelo();
+    dibujarLimon();
 }
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="darkgreen";
@@ -27,11 +33,19 @@ function dibujarPersonaje(){
     ctx.fillStyle="#265E6E";
     ctx.fillRect(personajeX,canvas.clientHeight-(ALTURA_SUELO+ALTURA_PERSONAJE),ACNCHO_PERSONAJE,ALTURA_PERSONAJE)
 }
+function dibujarLimon(){
+    ctx.fillStyle="#45b329";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
 function moverIzquierda(){
     personajeX=personajeX-20
     actualizarPantalla();
 }
 function moverDerecha(){
     personajeX=personajeX+20
+    actualizarPantalla();
+}
+function bajarLimon(){
+    limonY=limonY+10;
     actualizarPantalla();
 }
